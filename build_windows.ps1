db34ca4fb69253af9fc9358e46fc45c125cd4cbf
tvnt09 <venkatanagateja.thota@xdlinx.space>
2026-04-14 15:43:21 +0530

    Remove unused files

--- build_windows.ps1
+++
@@ -1,7 +0,0 @@
-$ErrorActionPreference = 'Stop'
-Set-Location -Path $PSScriptRoot
-
-py -m pip install --upgrade pyinstaller
-py build_exe.py
-
-Write-Host "Windows build complete: $PSScriptRoot\dist\DisplayGroundx\DisplayGroundx.exe"
